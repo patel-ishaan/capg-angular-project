786d35f010b38d8affd4912a32711b52b9e8db06
Navneet Dhar Dubey <navneetdhardubey.india@gmail.com>
2026-06-10 11:44:30 +0530

    Profile page progress

--- insurance-system/src/app/app.routes.ts
+++ insurance-system/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LandingPageComponent } from './pages/landing-page/landing-page';
 import { LoginComponent } from './components/login/login';
 import { RegisterComponent } from './components/register/register';
 import { DashboardComponent } from './dashboard/dashboard';
+import { ProfileComponent } from './components/profile/profile';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent},
     {path: 'login', component:LoginComponent},
     {path: 'register',component:RegisterComponent},
-    {path: 'dashboard',component:DashboardComponent}
+    {path: 'dashboard',component:DashboardComponent},
+    {path: 'profile',component: ProfileComponent}
 ];
